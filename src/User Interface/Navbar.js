6015a795fe6cb8121b3a05a9bd0a263fbb91874d
kingmic9 <sendtomichaeljas@gmail.com>
2024-09-15 20:10:53 -0400

    Basic Navbar Prototype
    - Added a basic Navbar that is slim, and should be modular with all the pages.
    - Shows which icon you're hovering over by making it larger and slightly change color.
    To do's: change color(s)
    - add link functionality when other pages are built.

---
+++ src/User Interface/Navbar.js
@@ -0,0 +1,32 @@
+// Navbar.js
+import React from 'react';
+import { FaHome, FaCompass, FaCalendarAlt, FaSearch } from 'react-icons/fa';
+import './Navbar.css'; // Import the CSS file for styling
+
+const Navbar = () => {
+  return (
+    <div className="sidebar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <FaHome className="nav-icon" />
+          <span className="nav-text">Home</span>
+        </li>
+        <li className="nav-item">
+          <FaCompass className="nav-icon" />
+          <span className="nav-text">Explore</span>
+        </li>
+        <li className="nav-item">
+          <FaCalendarAlt className="nav-icon" />
+          <span className="nav-text">Calendar</span>
+        </li>
+        <li className="nav-item">
+          <FaSearch className="nav-icon" />
+          <span className="nav-text">Search</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
+
