eaefba2d0501507f62aa0e2022573536f8912a3e
Tymofiy Sompura <sompurat@mail.utoronto.ca>
2024-09-15 22:46:16 -0400

    Add PostComments function

--- src/User Interface/Post.js
+++ src/User Interface/Post.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+export function PostComments() {
+    return (
+        <div></div>
+    );
+}
+
 export default function Post() {
     const [commentsHidden, setCommentsHidden] = useState(true);
 
