c9ae52ab21e344574be3aba67a869a5a26a1b1e4
kingmic9 <sendtomichaeljas@gmail.com>
2024-09-18 00:09:13 -0400

    Added Buttons to Posts
    - added the like, comment and share buttons to posts.
    -added color transitions and animations when hovering over the buttons
    -added a tooltip pop-up / description for when the button is hovered over as well.

--- src/User Interface/Post.js
+++ src/User Interface/Post.js
@@ -2,6 +2,7 @@ import React from 'react';
 import parse from 'html-react-parser';
 import { useState } from "react";
 import './Post.css';
+import { FaComment, FaHeart, FaPaperPlane } from 'react-icons/fa';
 
 export function PostComments() {
     return (
@@ -17,13 +18,29 @@ const Post =  ({post_data}) => {
     const [commentsHidden, setCommentsHidden] = useState(true);
 
     return (
+        
         <div className="post-box">
-            <div className='post-user-handle-row'>
-            <div className='post-username'>{post_data.username}</div>
-            <div className='post-handle'> &nbsp;&#183; {post_data.handle}</div>
+                <div className='post-user-handle-row'>
+                <div className='post-username'>{post_data.username}</div>
+                <div className='post-handle'> &nbsp;&#183; {post_data.handle}</div>
+                </div>
+                <p>{parse(post_data.content)}</p>
+                <div className='button-row'>
+                <div className='button-container'>
+                <FaHeart className='icon-button' />
+                <span class="tooltip">Like this post</span>
+                </div>
+                <div className='button-container'>
+                <FaComment className='icon-button' />
+                <span class="tooltip">Comment on this post</span>
+                </div>
+                <div className='button-container'>
+                <FaPaperPlane className='icon-button' />
+                <span class="tooltip">Share this post</span>
+                </div>
+                </div>
             </div>
-            <p>{parse(post_data.content)}</p>
-        </div>
+            
     );
 };
 
