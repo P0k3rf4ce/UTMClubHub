2ffead57ae6eefa69ff7d417f0247ca25126b11c
kingmic9 <sendtomichaeljas@gmail.com>
2024-09-29 13:26:26 -0400

    Revert "temp store changes"
    This reverts commit 76902166e2b573258eb0f448270874ada0fa9a38.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './User Interface/Home';
-import Explore from './User Interface/Explore';
-import Calendar from './User Interface/Calendar';
-import Search from './User Interface/Search';
-import Settings from './User Interface/Settings';
-import Account from './User Interface/Account';
-import Navbar from './User Interface/Navbar'; // Import Navbar
+import { useState } from "react";
+import Navbar from "./User Interface/Navbar";
+import Post from "./User Interface/Post";
+import postData from './User Interface/test/fake_posts.json';
+import Search from "./User Interface/Search";
 
+export default function App() {
+  // Use the correct syntax for declaring and using state
+  const [isPostVisible, setIsPostVisible] = useState(true);
+  const [isSearchVisible, setIsSearchVisible] = useState(false); // Initially, posts are shown, search is hidden
 
+  // Function to hide posts and show search
+  const post2search = () => {
+    setIsPostVisible(false);
+    setIsSearchVisible(true);
+  };
 
-const App = () => {
+  // Function to hide search and show posts
+  const search2post = () => {
+    setIsSearchVisible(false);
+    setIsPostVisible(true);
+  };
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
-    </Router>
+    <div>
+      <div>
+        <Navbar post2search={post2search} search2post={search2post} />
+      </div>
+      <div className="row">
+        <div className="post-section">
+          {/* Conditional rendering of posts */}
+          {isPostVisible && postData.map((post) => (<Post key={post.id} post_data={post} />))}
+        </div>
+      </div>
+      <div>
+        {/* Conditional rendering of search */}
+        {isSearchVisible && <Search />}
+      </div>
+    </div>
   );
-};
-
-export default App;
-
+}
--- src/User Interface/Navbar.js
+++ src/User Interface/Navbar.js
@@ -1,61 +1,34 @@
 // Navbar.js
 import React from 'react';
-import { FaHome, FaCompass, FaCalendarAlt, FaSearch, FaUsernp} from 'react-icons/fa'; // Add FaEye icon for the toggle
+import { FaHome, FaCompass, FaCalendarAlt, FaSearch, FaUser, FaEye } from 'react-icons/fa'; // Add FaEye icon for the toggle
 import './Navbar.css'; 
 import { FaGear } from 'react-icons/fa6';
-import { useNavigate } from 'react-router-dom';
 
-export default function Navbar() {
-  const navigate = useNavigate(); // useNavigate for navigation
-
-  const handleClickHome = () => {
-    navigate('/'); // Navigate to the Home page
-  };
-
-  const handleClickExplore = () => {
-    navigate('/explore'); // Navigate to the Explore page
-  };
-
-  const handleClickCalendar = () => {
-    navigate('/calendar'); // Navigate to the Calendar page
-  };
-
-  const handleClickSearch = () => {
-    navigate('/search'); // Navigate to the Search page
-  };
-  
-  const handleClickSettings = () => {
-    navigate('/settings'); // Navigate to the Settings page
-  };
-
-  const handleClickUser = () => {
-    navigate('/account'); // Navigate to the Account page
-  };
-
+export default function Navbar({post2search, search2post}) {
   return (
     <div className="sidebar">
       <ul className="nav-list">
-        <li className="nav-item" onClick={handleClickHome}>
+        <li className="nav-item" onClick={search2post}>
           <FaHome className="nav-icon" />
           <span className="nav-text">Home</span>
         </li>
-        <li className="nav-item" onClick={handleClickExplore}>
+        <li className="nav-item">
           <FaCompass className="nav-icon" />
           <span className="nav-text">Explore</span>
         </li>
-        <li className="nav-item" onClick={handleClickCalendar}>
+        <li className="nav-item">
           <FaCalendarAlt className="nav-icon" />
           <span className="nav-text">Calendar</span>
         </li>
-        <li className="nav-item" onClick={handleClickSearch}>
+        <li className="nav-divider-item" onClick={post2search}>
           <FaSearch className="nav-icon" />
           <span className="nav-text">Search</span>
         </li>
-        <li className="nav-item" onClick={handleClickSettings}>
+        <li className="nav-user-item">
           <FaGear className="nav-icon" />
           <span className="nav-text">Settings</span>
         </li>
-        <li className="nav-item" onClick={handleClickUser}>
+        <li className="nav-user-item">
           <FaUser className="nav-icon" />
           <span className="nav-text">Account</span>
         </li>
