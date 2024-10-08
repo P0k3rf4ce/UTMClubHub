cd0033e24ed035fc3410eb392e60fe2e9bce42ab
kingmic9 <sendtomichaeljas@gmail.com>
2024-10-07 22:28:18 -0400

    Small Calendar UI attempt
    nothing too crazy

--- src/User Interface/CalendarPage.js
+++ src/User Interface/CalendarPage.js
@@ -1,11 +1,52 @@
 import react from "react";
 import Navbar from "./Navbar";
+import "./CalendarPage.css"
 
 export default function CalendarPage(){
     return (
       <div>
           <Navbar></Navbar>
             <p> The Calendar Should go here!</p>
-      </div>
+            <div class="div-table">
+            <div class="div-table-caption">Top Portion</div>
+            <div class="div-table-row">
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            </div>
+            <div class="div-table-row">
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            </div>
+            <div class="div-table-row">
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            </div>
+            <div class="div-table-row">
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            <div class="div-table-col">Next Down</div>
+            </div>
+            </div>
+            </div>
+            
     );
   }
